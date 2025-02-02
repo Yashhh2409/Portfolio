9def5ccda6d64aabd186a7e70c74e3f634c79b5e
Yash Prashant Dhande <138235962+Yashhh2409@users.noreply.github.com>
2025-02-02 13:35:34 +0530

    Your commit message here

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/about.jpg"
-import CV from "../../assets/YashDhande_Resume.pdf"
+import CV from "../../assets/yash_resume.pdf"
 import Info from './Info'
 
 //motion
